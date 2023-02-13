0025654fc0689c75fbaa06edcc2faa815322eaef
Maurice Preiß <maurice.preiss2@gmail.com>
2023-02-13 14:10:51 +0100

    changed to long

--- FileSizeConverter/Converter.cs
+++ FileSizeConverter/Converter.cs
@@ -22,12 +22,12 @@ namespace FileSizeConverter
         /// <summary>Convert Bytes To Wanted Size Unit</summary>
         /// <param name="unit">Unit (GB, MB, ...)</param>
         /// <param name="state">State for Unit Letters</param>
-        public static string ToSize(this Int64 value, SizeUnits unit, DisplayUnitState state)
+        public static string ToSize(this long value, SizeUnits unit, DisplayUnitState state)
         {
             if (state == DisplayUnitState.Shown)
-                return (value / (double)Math.Pow(1024, (Int64)unit)).ToString("0.00" + unit);
+                return (value / (double)Math.Pow(1024, (long)unit)).ToString("0.00" + unit);
             else if (state == DisplayUnitState.Hidden)
-                return (value / (double)Math.Pow(1024, (Int64)unit)).ToString("0.00");
+                return (value / (double)Math.Pow(1024, (long)unit)).ToString("0.00");
             else
                 throw new Exception("Not a valid DisplayUnitState");
         }
